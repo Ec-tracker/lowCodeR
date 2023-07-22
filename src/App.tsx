65ad81698e983c749ff47903a83f870d1193d851
e <jabezliu@gmail.com>
2023-07-22 08:53:41 +0800

    添加初始文件

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import EditPage from './pages/EditPage'
+import ListPage from './pages/ListPage'
+import RequireAuth from './components/RequireAuth'
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<RequireAuth />}>
+          <Route index element={<EditPage />} />
+          <Route path="list" element={<ListPage />} />
+        </Route>
+      </Routes>
+    </Router>
+  )
+}
